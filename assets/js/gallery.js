98e83b34e983c18d92b918bf276cb59c0eec371c
Saket Sarin <sarinsaket@gmail.com>
2022-11-12 23:04:33 +0530

    Update gallery.js

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -2,9 +2,15 @@ document.querySelectorAll(".g-img-container").forEach(function (el) {
   el.addEventListener("click", showPreview);
 });
 
+var curImg;
+
 function showPreview() {
-  var src = $(this).children("img").attr("src");
+  curImg && curImg.css("border", "none");
+  curImg = $(this).children("img");
+  var src = curImg.attr("src");
   $("#previewImg").children("img").attr("src", src);
+
+  curImg.css("border", "3px solid #896eff");
 
   document
     .getElementById("previewContainer")
@@ -15,6 +21,7 @@ function showPreview() {
 
 function closePreview() {
   document.getElementById("gallery").style.width = "100%";
+  curImg.css("border", "none");
   document
     .getElementById("previewContainer")
     .classList.remove("preview-container-show");
